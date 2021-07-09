ed76fb36355fac1c3f828e02233a468132292435
jsprofessional <jshuman@protonmail.com>
2021-07-09 16:59:06 -0500

    Table is set in server file, rendering in templates

--- server.py
+++ server.py
@@ -4,7 +4,13 @@ app = Flask(__name__)
 
 @app.route('/index')
 def hello_world():
-    return render_template('index.html')
+    users = [
+    {'first_name' : 'Michael', 'last_name' : 'Choi'},
+    {'first_name' : 'John', 'last_name' : 'Supsupin'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+    return render_template('index.html', usertable = users)
 
 if __name__ == "__main__":
     app.run(debug = True)
